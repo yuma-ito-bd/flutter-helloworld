94585c1ce7209f0147cfde9de56014b1f815ab73
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2024-02-24 12:24:35 +0900

    アプリバーにアイコンを追加する

--- lib/main.dart
+++ lib/main.dart
@@ -65,14 +65,18 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
         appBar: AppBar(
-          // TRY THIS: Try changing the color here to a specific color (to
-          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-          // change color while the other colors stay the same.
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-        ),
+            // TRY THIS: Try changing the color here to a specific color (to
+            // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+            // change color while the other colors stay the same.
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            // Here we take the value from the MyHomePage object that was created by
+            // the App.build method, and use it to set our appbar title.
+            title: const Row(
+              children: [
+                Icon(Icons.create),
+                Text("初めてのタイトル"),
+              ],
+            )),
         body: Column(
           children: [
             const Text("Hello World!"),
