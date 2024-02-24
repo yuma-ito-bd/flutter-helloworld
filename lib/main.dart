2fc20d7f0be36bf35c04f92e0a9e58155cd89b1a
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2024-02-24 12:13:52 +0900

    テキストボタン要素を追加

--- lib/main.dart
+++ lib/main.dart
@@ -73,8 +73,14 @@ class _MyHomePageState extends State<MyHomePage> {
           // the App.build method, and use it to set our appbar title.
           title: Text(widget.title),
         ),
-        body: const Column(
-          children: [Text("Hello World!"), Text("ハローワールド")],
+        body: Column(
+          children: [
+            const Text("Hello World!"),
+            const Text("ハローワールド"),
+            TextButton(
+                onPressed: () => {print("ボタンが押された")}, 
+                child: const Text("ボタン"))
+          ],
         ));
   }
 }
